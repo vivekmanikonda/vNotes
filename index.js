a06f9953edbcb30e4f2487bbb4c94d4cd1fb0774
vivekmanikonda <saivivekmanikonda11@gmail.com>
2024-12-01 20:22:42 +0530

    js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+const notesContainer = document.querySelector(".notes-container"); // Container for notes
+
+function addNote() {
+    let input = document.createElement("p");
+    const img = document.createElement("img");
+    img.src = "images/DelIcon.png";
+    input.className = "notes";
+    input.setAttribute("contenteditable", "true");
+    notesContainer.appendChild(input).appendChild(img);
+    img.addEventListener("click", () => {
+        input.remove();
+    });
+};
+
